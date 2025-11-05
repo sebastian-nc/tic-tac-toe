d6e85fca82ff6178855bf3e86089de6e2a82bbad
sebastian <scsebastian1406@gmail.com>
2025-11-05 15:30:54 -0500

    add v2 to game

--- src/App.jsx
+++ src/App.jsx
@@ -3,72 +3,51 @@ import './App.css'
 
 function App() {
 
-  const [turn, setTurn] = useState(0) // player 1
-  const [charts, setCharts] = useState(new Array(9).fill(null))
+  const [turn, setTurn] = useState(0) // 1 = player 1, 2 = player 2
+  const [squares, setSquares] = useState(new Array(9).fill(null))
   const [playerWin, setPlayerWin] = useState(null)
   const winningCombination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 
   const players = {
-    "1": {
+    0: {
       alias: "Jugador 1",
       icon: "✖️"
     },
-    "2": {
+    1: {
       alias: "Jugador 2",
       icon: "⭕"
     }
   }
 
-  function searchWinPlayer(winningCombination, playerCombination) {
-    for (let index = 0; index < winningCombination.length; index++) {
-      const winning = winningCombination[index];
-      let win = 0
-      for (let index = 0; index < playerCombination.length; index++) {
-        const element = playerCombination[index];
-        if (winning.includes(element)) {
-          win += 1
-        }
-      }
-
-      if (win == 3) {
-        return true
-        // setPlayerWin(player.alias)
-        // break
-      }
-    }
+  function checkWin(board, icon){
+    return winningCombination.some(combo => combo.every(i => board[i] === icon))
   }
 
-  function selectChart(prop, index) {
-    if (charts[index] !== null || playerWin) {
+
+  function selectChart(_, index) {
+    if (squares[index] !== null || playerWin) return
+    const currentPlayer = players[turn]
+    const newSquares = [...squares]
+    newSquares[index] = currentPlayer.icon
+    setSquares(newSquares)
+
+    if (checkWin(newSquares, currentPlayer.icon)) {
+      setPlayerWin(currentPlayer.alias)
       return
     }
 
-    const player = turn == false ? players[1] : players[2]
-    const copyCharts = charts
-    copyCharts[index] = player.icon
-    setCharts(copyCharts)
-
-    const playerCombination = []
-
-    charts.filter((plays, index) => {
-      if (plays == player.icon) {
-        playerCombination.push(index)
-      }
-    })
-
-    const winPlayer = searchWinPlayer(winningCombination, playerCombination)
-
-    if (winPlayer) {
-      setPlayerWin(player.alias)
-    } else {
-      setTurn(!turn)
+    if(newSquares.every(cell => cell != null)) {
+      setPlayerWin('Empate')
+      return
     }
+
+    setTurn(1 - turn)
   }
 
   function resetGame() {
-    charts.fill(null)
-    setTurn(false)
+    setSquares(new Array(9).fill(null))
+    setTurn(0)
     setPlayerWin(null)
   }
 
@@ -77,16 +56,16 @@ function App() {
       <h1>Tic Tac Toe</h1>
       <div className='container-players'>
         <div className={`${turn == 0 && 'player-select'}`}>
-          <span className='player'>{players[1].alias} | {players[1].icon}</span>
+          <span className='player'>{players[0].alias} | {players[0].icon}</span>
         </div>
         <div className={`${turn == 1 && 'player-select'}`}>
-          <span className='player'>{players[2].alias} | {players[2].icon}</span>
+          <span className='player'>{players[1].alias} | {players[1].icon}</span>
         </div>
       </div>
 
       <div className={`container-game ${playerWin && 'container-game-final'}`}>
         {
-          charts.map((chart, index) => {
+          squares.map((chart, index) => {
             return (
               <div
                 key={index}
